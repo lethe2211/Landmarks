07ca8a95b583a08c46385e22960c0ac96f6abd75
lethe2211 <lethe2211@gmail.com>
2021-02-16 15:42:04 +0900

    Initial Commit

---
+++ Shared/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Shared
+//
+//  Created by Shogen, Shuhei | Lethe | ELPD on 2021/02/16.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
