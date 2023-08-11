4be59cd521247fdf1a8d036b2833745cd5bffcee
lethe2211 <lethe2211@gmail.com>
2023-08-11 09:33:24 +0900

    WIP

---
+++ Shared/View/Landmarks/LandmarkRow.swift
@@ -0,0 +1,41 @@
+//
+//  LandmarkRow.swift
+//  Landmarks (iOS)
+//
+//  Created by Shogen, Shuhei | Lethe | ELPD on 2021/02/28.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+
+    var body: some View {
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+
+            Spacer()
+            
+            if landmark.isFavorite {
+                Image(systemName: "star.fill")
+                    .foregroundColor(.yellow)
+            }
+        }
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var landmarks = ModelData().landmarks
+
+    static var previews: some View {
+        Group {
+            LandmarkRow(landmark: landmarks[0])
+                .previewLayout(.fixed(width: 300, height: 70))
+            LandmarkRow(landmark: landmarks[1])
+                .previewLayout(.fixed(width: 300, height: 70))
+        }
+    }
+}
